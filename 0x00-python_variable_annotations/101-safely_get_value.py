5f259c1ecdd002689427e8cc4437a015a2a5d011
Tony Mputhia <tonymmputhia@gmail.com>
2024-05-03 05:27:56 +0300

    More involved type annotations

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Module for safely retrieving a value from a dictionary with a default.
+
+"""
+
+from typing import Mapping, Any, TypeVar, Optional
+
+# Type variable that could be any type or None
+T = TypeVar('T')
+
+def safely_get_value(dct: Mapping[Any, T], key: Any, default: Optional[T] = None) -> Optional[T]:
+    """
+    Get the value from a dictionary by key or return a default value.
+
+    """
+    if key in dct:
+        return dct[key]
+    else:
+        return default
