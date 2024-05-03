90dbb8605cf0ec8e2295b433b82fef0363183147
Tony Mputhia <tonymmputhia@gmail.com>
2024-05-03 04:59:38 +0300

    Use basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module for adding two floating-point numbers.
+
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    Add two float numbers and return the sum.
+    
+    """
+    return a + b
