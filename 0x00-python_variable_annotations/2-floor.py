1a5f71987b891068d9a7ef0747182d26f545999d
Tony Mputhia <tonymmputhia@gmail.com>
2024-05-03 05:03:47 +0300

    Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Module for computing the floor of a floating-point number.
+
+"""
+
+import math
+
+def floor(n: float) -> int:
+    """
+    Compute the floor of a floating-point number and return it.
+    
+    """
+    return math.floor(n)
