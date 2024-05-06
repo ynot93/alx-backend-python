ff47acb7b33d016ec8ce348d14ea4b9b95ecbcd6
Tony Mputhia <tonymmputhia@gmail.com>
2024-05-06 15:25:33 +0300

    Basics of async

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+This module tests the basics of async coroutines
+
+"""
+import random
+import asyncio
+
+async def wait_random(max_delay=10):
+    """
+    This function waits for random delay and eventually
+    returns the coroutine
+
+    """
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
