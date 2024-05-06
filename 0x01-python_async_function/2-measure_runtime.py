4f3873b08b69b9ad0e4dcf5e8b6f9f43a8f7cdc7
Tony Mputhia <tonymmputhia@gmail.com>
2024-05-06 19:38:55 +0300

    Measue runtime of async tasks

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+This module tests the basics of async coroutines
+
+"""
+import asyncio
+import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+async def measure_time(n, max_delay):
+    """
+    This function measures the time executing wait_n takes
+    
+    """
+    start_time = time.time()
+
+    await wait_n(n, max_delay)
+
+    end_time = time.time()
+    
+    total_time = end_time - start_time
+        
+    return total_time / n
