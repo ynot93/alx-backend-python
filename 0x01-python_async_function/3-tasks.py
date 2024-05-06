b8f2aa4fb1f4d5c17aa29d45faecd87f31cf9cc3
Tony Mputhia <tonymmputhia@gmail.com>
2024-05-06 19:39:15 +0300

    Use asyncio.Task

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+This module tests the basics of async coroutines
+
+"""
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+def task_wait_random(max_delay):
+    """
+    Returns a asyncio.Task object
+    
+    """
+    return asyncio.Task(wait_random(max_delay))
+    
