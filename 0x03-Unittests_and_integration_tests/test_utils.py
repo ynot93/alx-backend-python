0853ac6e0c11316cccb5fce2a17d0d3a1079987c
Tony Mputhia <tonymmputhia@gmail.com>
2024-05-26 22:21:00 +0300

    Fix pycodestyle error

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -19,10 +19,7 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_app(self,
-                               nested_map: Mapping,
-                               path: Sequence,
-                               expected: Any) -> None:
+    def test_access_nested_app(self, nested_map: Mapping, path: Sequence, expected: Any) -> None:
         """
         Test correct return value of method
 
