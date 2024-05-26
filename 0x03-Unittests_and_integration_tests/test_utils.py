621d5958d55a1124c465943f735f9731e891499b
Tony Mputhia <tonymmputhia@gmail.com>
2024-05-26 22:22:42 +0300

    Fix syntax error

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -17,7 +17,7 @@ class TestAccessNestedMap(unittest.TestCase):
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2)
+        ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_app(self, nested_map: Mapping, path: Sequence, expected: Any) -> None:
         """
