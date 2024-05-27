162ce257cb0234fd6e6613aec79416cfac8e9172
Tony Mputhia <tonymmputhia@gmail.com>
2024-05-28 00:28:05 +0300

    Mock a property object

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -28,3 +28,16 @@ class TestGithubOrgClient(unittest.TestCase):
 
         self.assertEqual(result, {"key": "value"})
         mock_get_json.assert_called_once_with(org_object)
+    
+    def test_public_repos_url(self):
+        """
+        Tests that this returns the correct URL based on the mocked payload
+
+        """
+        payload = {"repos_url": "https://api.github.com/orgs/test_org/repos"}
+        
+        with patch.object(GithubOrgClient, 'org', return_value=payload):
+            client = GithubOrgClient("test_org")
+            result = client._public_repos_url
+
+            self.assertEqual(result, "https://api.github.com/orgs/test_org/repos")
